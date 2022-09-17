5b9de57075ac9806b4595cf11023539673777b30
Hose-Soh <sohrawardy.mhose@gmail.com>
2022-09-17 14:13:34 +0400

    Career Tracks/

---
+++ Career Tracks/Python Programmer/1. Introduction to Data Science in Python/1. Getting Started in Python/2_Correcting a broken import.py
@@ -0,0 +1,5 @@
+# Fix the import of numpy to run without errors
+import numpy as np
+
+#What did you need to change to make the import run without errors?
+#Python is case-sensitive, so numpy must be all lowercase.
---
+++ Career Tracks/Python Programmer/1. Introduction to Data Science in Python/2. Loading Data in pandas/2_Inspecting a DataFrame.py
@@ -0,0 +1,4 @@
+# Use .info() to inspect the DataFrame credit_records
+print(credit_records.info())
+
+
