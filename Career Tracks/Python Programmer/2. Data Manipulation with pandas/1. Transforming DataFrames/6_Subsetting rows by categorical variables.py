a0b7da5f414471b5eeef521f172486b6919fb801
Hose-Soh <sohrawardy.mhose@gmail.com>
2022-09-20 22:48:16 +0400

    Career Tracks/

---
+++ Career Tracks/Python Programmer/2. Data Manipulation with pandas/1. Transforming DataFrames/6_Subsetting rows by categorical variables.py
@@ -0,0 +1,19 @@
+# Subset for rows in South Atlantic or Mid-Atlantic regions
+
+#reg = ["South Atlantic", "Mid-Atlantic"] 
+
+south_mid_atlantic = homelessness[(homelessness["region"] == "South Atlantic") | (homelessness["region"] == "Mid-Atlantic")]
+
+# See the result
+print(south_mid_atlantic)
+
+###################################
+
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+
+# Filter for rows in the Mojave Desert states
+mojave_homelessness = homelessness[homelessness["state"].isin(canu)]
+
+# See the result
+print(mojave_homelessness)
